b26e7219d648d1056b854686889a500ff0bbba16
ngthvan1612 <ngthvan1612@gmail.com>
2022-11-03 22:46:54 +0700

    Thêm hàm sinh mã JWT

--- LFF.API/Helpers/Authorization/GenerateJwtToken.cs
+++ LFF.API/Helpers/Authorization/GenerateJwtToken.cs
@@ -9,13 +9,13 @@ namespace LFF.API.Helpers.Authorization
 {
     public static class GenerateJwtToken
     {
-        public static string GetJwtToken(User user, AppSettings _appSettings)
+        public static string GetJwtToken(User? user, AppSettings _appSettings)
         {
             var tokenHandler = new JwtSecurityTokenHandler();
             var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
             var tokenDescriptor = new SecurityTokenDescriptor
             {
-                Subject = new ClaimsIdentity(new[] { new Claim("Id", user.Id.ToString()) }),
+                Subject = new ClaimsIdentity(new[] { new Claim("Id", user?.Id.ToString()) }),
                 Expires = DateTime.UtcNow.AddDays(7),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
             };
