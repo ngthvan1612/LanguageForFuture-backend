c7e6cca9d5d93d8b952802c6c19602f7516addb4
ngthvan1612 <ngthvan1612@gmail.com>
2022-11-03 17:30:14 +0700

    Thêm appSetting (JWT)

---
+++ LFF.API/Helpers/AppSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace LFF.API.Helpers
+{
+    public class AppSettings
+    {
+        public AppSettings(string secret, string issuer, string audience)
+        {
+            Secret = secret;
+            Issuer = issuer;
+            Audience = audience;
+        }
+
+        public string Secret { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+    }
+}
