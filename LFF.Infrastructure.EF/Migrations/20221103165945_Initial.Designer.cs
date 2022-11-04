ee3e087cd75e4847f588c5345a1857dd3dd405a3
ngthvan1612 <ngthvan1612@gmail.com>
2022-11-04 10:46:49 +0700

    Thực hiện migration lại từ đầu

--- LFF.Infrastructure.EF/Migrations/20221103165945_Initial.Designer.cs
+++ LFF.Infrastructure.EF/Migrations/20221103165945_Initial.Designer.cs
@@ -4,14 +4,16 @@ using LFF.Infrastructure.EF.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LFF.Infrastructure.EF.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221103165945_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
