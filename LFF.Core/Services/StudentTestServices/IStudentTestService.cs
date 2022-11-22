5474a1725f1f4d0da044d8feacadadaa38cb58a9
ngthvan1612 <ngthvan1612@gmail.com>
2022-11-22 22:05:24 +0700

    Thêm service xử lý việc  Id student và id bài kiểm tra trả về cho nó 1 danh sách các kết quả làm bài của nó đối với bài kiểm tra đó

--- LFF.Core/Services/StudentTestServices/IStudentTestService.cs
+++ LFF.Core/Services/StudentTestServices/IStudentTestService.cs
@@ -14,5 +14,6 @@ namespace LFF.Core.Services.StudentTestServices
         Task<ResponseBase> GetStudentTestByIdAsync(Guid id);
         Task<ResponseBase> ListStudentTestAsync(IEnumerable<SearchQueryItem> queries);
         Task<ResponseBase> DeleteStudentTestByIdAsync(Guid id);
+        Task<ResponseBase> GetStudentTestHistory(Guid studentId, Guid testId);
     }
 }
