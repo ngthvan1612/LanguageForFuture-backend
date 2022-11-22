28e395e4126429328eb4023c20a0cfd471296d29
ngthvan1612 <ngthvan1612@gmail.com>
2022-11-22 22:08:15 +0700

    Update ITestRepository.cs

--- LFF.Core/Repositories/ITestRepository.cs
+++ LFF.Core/Repositories/ITestRepository.cs
@@ -1,4 +1,5 @@
 using LFF.Core.Entities;
+using LFF.Core.Entities.Supports;
 using System;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace LFF.Core.Repositories
     public interface ITestRepository : IRepository<Test>
     {
         Task<Test> GetTestByIdAsync(Guid id);
+        Task<StudentTestHistory> GetStudentTestHistory(Guid studentId, Guid testId);
         Task<bool> CheckTestExistedByIdAsync(Guid id);
     }
 }
